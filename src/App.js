1527caf5464db33b05e85d79dab53431338f13a1
DeviPothula <devikapothula597@gmail.com>
2024-10-21 12:11:21 +0530

    First COmmit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return <div className="App">
+    <h1>I am deploying my first react app to EC2 linux instance</h1>
+    <p>Hi devi</p>
+  </div>;
+}
+
+export default App;
