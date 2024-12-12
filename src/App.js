d9a1d4cdd0b87a2671e1d8ee20a41062bb0313a3
DeviPothula <devikapothula597@gmail.com>
2024-12-12 15:57:27 +0530

    docker added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
   return <div className="App">
     <h1>I am Deploying My First React-App to EC2 Instance</h1>
     <p>Hi Devi!!</p>
+    <h3>Here I am Using AWS Code Build and Code Pipeline</h3>
   </div>;
 }
 
