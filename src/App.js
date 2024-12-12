9ce907185ebf337754d8fe81f9cf64b79f2fd049
DeviPothula <devikapothula597@gmail.com>
2024-12-12 17:11:21 +0530

    mew change to test code pipeline

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
     <h1>I am Deploying My First React-App to EC2 Instance</h1>
     <p>Hi Devi!!</p>
     <h3>Here I am Using AWS Code Build and Code Pipeline</h3>
+    <h4>Code build added to code pipeline....</h4>
   </div>;
 }
 
