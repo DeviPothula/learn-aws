15522ac1fcfceeaeac47dc844d6f8e859ace45de
DeviPothula <devikapothula597@gmail.com>
2024-12-13 14:06:51 +0530

    aws ecr login added

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,3 +1,14 @@
+#!/bin/bash
+
+# Update system packages
+sudo yum update -y
+
+# Install Docker
+echo "Installing Docker..."
 sudo curl -fsSL https://get.docker.com -o install-docker.sh
-sudo sh install-docker.sh --dry-run
 sudo sh install-docker.sh
+
+# Start and enable Docker
+echo "Starting Docker..."
+sudo systemctl start docker
+sudo systemctl enable docker
--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,6 +5,8 @@ IMAGE="148761656970.dkr.ecr.ap-south-1.amazonaws.com/react-app-namespace/react-a
 PORT=3000
 
 #commands 
+echo "login to aws"
+aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 148761656970.dkr.ecr.ap-south-1.amazonaws.com
 echo "i am going to run my docker image"
 docker pull $IMAGE
 docker run -d -p 3000:3000 $IMAGE
